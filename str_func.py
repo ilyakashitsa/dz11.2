cb5bb29ada32d76603938a4c48b4f16a0628a381
iliag <ilia.@yandex.ru>
2024-02-08 17:01:11 +0300

    new feature

--- str_func.py
+++ str_func.py
@@ -2,6 +2,6 @@ def all_caps(input_string):
     return input_string.upper()
 
 
-input_str = input("Пример строки с разным регистром букв")
+input_str = input("Строка с разным регистром букв")
 result = all_caps(input_str)
 print(result)
