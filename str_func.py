6e272b1db2f8ddc439d3f8b3ed1063539e0ef7c8
iliag <ilia.@yandex.ru>
2024-02-08 17:21:50 +0300

    new feature first big docstring

--- str_func.py
+++ str_func.py
@@ -1,6 +1,8 @@
 def all_caps(input_string):
+    """ВСЕ ЗАГЛАВНЫЕ"""
     return input_string.upper()
 def capitalize_words(input_string):
+    """ПЕРВАЯ ЗАГЛАВНАЯ БУКВА"""
     return input_string.title()
 
 input_str = input("Строка с разным регистром букв")
