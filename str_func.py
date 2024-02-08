b652539158a530a91d33bb88b16696aacc5c506c
iliag <ilia.@yandex.ru>
2024-02-08 17:19:04 +0300

    new feature first big

--- str_func.py
+++ str_func.py
@@ -1,7 +1,13 @@
 def all_caps(input_string):
     return input_string.upper()
-
+def capitalize_words(input_string):
+    return input_string.title()
 
 input_str = input("Строка с разным регистром букв")
 result = all_caps(input_str)
 print(result)
+
+input_string = input("Строки для теста")
+result = capitalize_words(input_string)
+print(result)
+
